aa07b7af53f55e9b0992acea28c0c3837b1438f4
Damir Tešnjak <damir.tesnjak@gmail.com>
2026-03-16 20:02:25 +0100

    L15_Find_max

---
+++ exercises/CH9_Lists/L15_Find_Max.py
@@ -0,0 +1,9 @@
+def find_max(nums):
+    max_so_far = float("-inf")
+    if len(nums) == 0:
+        return max_so_far
+    for num in nums:
+        if num > max_so_far:
+            max_so_far = num
+    return max_so_far
+    
