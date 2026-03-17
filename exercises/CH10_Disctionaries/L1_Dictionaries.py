276c4f195b6d7f0e4f7ba4d2a7dfd6aa3b13a503
Damir Tešnjak <damir.tesnjak@gmail.com>
2026-03-17 16:33:19 +0100

    L1_Dictionaries

---
+++ exercises/CH10_Disctionaries/L1_Dictionaries.py
@@ -0,0 +1,10 @@
+def get_character_record(name, server, level, rank):
+    player = {
+        "name": name,
+        "server": server,
+        "level": level,
+        "rank": rank,
+        "id": f"{name}#{server}"
+    }
+
+    return player
